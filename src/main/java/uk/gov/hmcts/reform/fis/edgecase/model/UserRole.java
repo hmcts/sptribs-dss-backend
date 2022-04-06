f4bc6a3f0f86ce46af76b11b260f8cd74793c0bb
praveenrach <praveen.rachapally@HMCTS.NET>
2022-04-06 14:11:22 +0100

    merged ISDB-116 code into this ISDB-111 branch

--- src/main/java/uk/gov/hmcts/reform/fis/edgecase/model/UserRole.java
+++ src/main/java/uk/gov/hmcts/reform/fis/edgecase/model/UserRole.java
@@ -5,25 +5,20 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import uk.gov.hmcts.ccd.sdk.api.HasRole;
 
-import static uk.gov.hmcts.reform.fis.edgecase.constants.CaseType.CRU;
-
 @AllArgsConstructor
 @Getter
 public enum UserRole implements HasRole {
-
-    ADOPTION_GENERIC("caseworker-adoption", CRU.toString()),
-    CASE_WORKER("caseworker-adoption-caseworker", CRU.toString()),
-
-    COURT_ADMIN("caseworker-adoption-courtadmin", CRU.toString()),
-    LEGAL_ADVISOR("caseworker-adoption-la", CRU.toString()),
-    DISTRICT_JUDGE("caseworker-adoption-judge", CRU.toString()),
-    SUPER_USER("caseworker-adoption-superuser", CRU.toString()),
-    SOLICITOR("caseworker-adoption-solicitor", CRU.toString()),
-    CITIZEN("citizen", "CRUD"),
-    CREATOR("[CREATOR]", CRU.toString());
+    ADOPTION_GENERIC(UserRoleConstant.ADOPTION_GENERIC, UserRoleConstant.CASE_TYPE_PERMISSIONS_CRU),
+    CASE_WORKER(UserRoleConstant.CASE_WORKER, UserRoleConstant.CASE_TYPE_PERMISSIONS_CRU),
+    COURT_ADMIN(UserRoleConstant.COURT_ADMIN, UserRoleConstant.CASE_TYPE_PERMISSIONS_CRU),
+    LEGAL_ADVISOR(UserRoleConstant.LEGAL_ADVISOR, UserRoleConstant.CASE_TYPE_PERMISSIONS_CRU),
+    DISTRICT_JUDGE(UserRoleConstant.DISTRICT_JUDGE, UserRoleConstant.CASE_TYPE_PERMISSIONS_CRU),
+    SUPER_USER(UserRoleConstant.SUPER_USER, UserRoleConstant.CASE_TYPE_PERMISSIONS_CRU),
+    SOLICITOR(UserRoleConstant.SOLICITOR, UserRoleConstant.CASE_TYPE_PERMISSIONS_CRU),
+    CITIZEN(UserRoleConstant.CITIZEN, UserRoleConstant.CASE_TYPE_PERMISSIONS_CRU),
+    CREATOR(UserRoleConstant.CREATOR, UserRoleConstant.CASE_TYPE_PERMISSIONS_CRUD);
 
     @JsonValue
     private final String role;
     private final String caseTypePermissions;
-
 }
