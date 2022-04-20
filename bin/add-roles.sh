3e603529b7c5110a86cbca71c812331e75e975a1
Mohammed Murshad Haque <murshad.haque@HMCTS.NET>
2022-04-20 15:18:26 +0100

    ISDB-112: Removed unnecessary roles

--- bin/add-roles.sh
+++ bin/add-roles.sh
@@ -3,14 +3,6 @@
 scriptPath=$(dirname $(realpath $0))
 
 # Roles used during the CCD import
-${scriptPath}/add-ccd-role.sh "caseworker-fis"
-${scriptPath}/add-ccd-role.sh "caseworker-fis-caseworker"
-${scriptPath}/add-ccd-role.sh "caseworker-fis-courtadmin"
-${scriptPath}/add-ccd-role.sh "caseworker-fis-la"
-${scriptPath}/add-ccd-role.sh "caseworker-fis-judge"
-${scriptPath}/add-ccd-role.sh "caseworker-fis-superuser"
-${scriptPath}/add-ccd-role.sh "caseworker-fis-solicitor"
-${scriptPath}/add-ccd-role.sh "citizen"
 ${scriptPath}/add-ccd-role.sh "caseworker-family-solicitor"
 ${scriptPath}/add-ccd-role.sh "caseworker-family-caseworker"
 ${scriptPath}/add-ccd-role.sh "caseworker-family"
@@ -18,3 +10,4 @@ ${scriptPath}/add-ccd-role.sh "caseworker-family-judge"
 ${scriptPath}/add-ccd-role.sh "caseworker-family-la"
 ${scriptPath}/add-ccd-role.sh "caseworker-family-superuser"
 ${scriptPath}/add-ccd-role.sh "caseworker-adoption-courtadmin"
+${scriptPath}/add-ccd-role.sh "citizen"
