690171670fd6140699ee8f44856d5554dec33aa7
saravanakumar-palanichamy <saravanakumar.palanichamy@HMCTS.NET>
2022-05-25 11:59:47 +0100

    removed secret from infra

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -15,9 +15,3 @@ data "azurerm_key_vault" "s2s_vault" {
   name                = "s2s-${var.env}"
   resource_group_name = "rpe-service-auth-provider-${var.env}"
 }
-
-resource "azurerm_key_vault_secret" "s2s-secret-fis-cos-api" {
-  name         = "s2s-secret-fis-cos-api"
-  value        = data.azurerm_key_vault_secret.microservicekey_fis_cos_api.value
-  key_vault_id = data.azurerm_key_vault.fis_key_vault.id
-}
