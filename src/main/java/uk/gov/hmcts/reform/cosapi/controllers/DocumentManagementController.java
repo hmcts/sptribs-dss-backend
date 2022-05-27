7b8106e3259b67d973e593e6f32c8b350a327dd6
saravanakumar-palanichamy <saravanakumar.palanichamy@HMCTS.NET>
2022-05-27 14:21:47 +0100

    checkstyle error

--- src/main/java/uk/gov/hmcts/reform/cosapi/controllers/DocumentManagementController.java
+++ src/main/java/uk/gov/hmcts/reform/cosapi/controllers/DocumentManagementController.java
@@ -7,7 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import uk.gov.hmcts.reform.cosapi.services.DocumentManagementService;
 
