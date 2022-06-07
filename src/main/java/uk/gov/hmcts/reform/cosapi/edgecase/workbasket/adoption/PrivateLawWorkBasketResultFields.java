9031643788e8687e4c7801c0d8b5f02b728cad70
saravanakumar-palanichamy <saravanakumar.palanichamy@HMCTS.NET>
2022-06-07 15:46:14 +0100

    removed unwanted ccd config files

--- src/main/java/uk/gov/hmcts/reform/cosapi/edgecase/workbasket/adoption/PrivateLawWorkBasketResultFields.java
+++ src/main/java/uk/gov/hmcts/reform/cosapi/edgecase/workbasket/adoption/PrivateLawWorkBasketResultFields.java
@@ -1,17 +1,17 @@
-package uk.gov.hmcts.reform.cosapi.edgecase.workbasket.privatelaw;
+package uk.gov.hmcts.reform.cosapi.edgecase.workbasket.adoption;
 
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.ccd.sdk.api.CCDConfig;
 import uk.gov.hmcts.ccd.sdk.api.ConfigBuilder;
-import uk.gov.hmcts.reform.cosapi.edgecase.model.PrivateLawCaseData;
+import uk.gov.hmcts.reform.cosapi.edgecase.model.CaseData;
 import uk.gov.hmcts.reform.cosapi.edgecase.model.State;
 import uk.gov.hmcts.reform.cosapi.edgecase.model.UserRole;
 
 @Component
-public class PrivatelawWorkBasketResultFields implements CCDConfig<PrivateLawCaseData, State, UserRole> {
+public class PrivateLawWorkBasketResultFields implements CCDConfig<CaseData, State, UserRole> {
 
     @Override
-    public void configure(final ConfigBuilder<PrivateLawCaseData, State, UserRole> configBuilder) {
+    public void configure(final ConfigBuilder<CaseData, State, UserRole> configBuilder) {
 
         configBuilder
             .workBasketResultFields()
