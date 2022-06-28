19f5007799f124b113691993c94b839e2a3202ee
Rahul Sanjava <rahul.sanjava@HMCTS.NET>
2022-06-28 12:39:49 +0100

    feat: update ContactPreference values (ISDB-248)

--- src/main/java/uk/gov/hmcts/reform/cosapi/model/ContactPreference.java
+++ src/main/java/uk/gov/hmcts/reform/cosapi/model/ContactPreference.java
@@ -1,22 +1,13 @@
 package uk.gov.hmcts.reform.cosapi.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import uk.gov.hmcts.ccd.sdk.api.HasLabel;
 
 @Getter
 @AllArgsConstructor
-public enum ContactPreference implements HasLabel {
+public enum ContactPreference {
 
-    @JsonProperty("ao")
-    ACCOUNT_OWNER("ao"),
-
-    @JsonProperty("np")
-    NAMED_PERSON("np"),
-
-    @JsonProperty("bre")
-    BOTH_RECEIVE("bre");
-
-    private final String label;
+    ACCOUNT_OWNER,
+    NAMED_PERSON,
+    BOTH_RECEIVE
 }
