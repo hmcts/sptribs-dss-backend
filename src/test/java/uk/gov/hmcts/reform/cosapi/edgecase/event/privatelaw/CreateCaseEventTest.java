4fb196d75f646c59a962bdb384bbb641af0a2709
Adetola Opanuga <2109031@cognizant.com>
2022-06-30 11:39:10 +0100

    ISDB-308 Updated new changes for test cases

--- src/test/java/uk/gov/hmcts/reform/cosapi/edgecase/event/privatelaw/CreateCaseEventTest.java
+++ src/test/java/uk/gov/hmcts/reform/cosapi/edgecase/event/privatelaw/CreateCaseEventTest.java
@@ -10,15 +10,20 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.ccd.sdk.ConfigBuilderImpl;
 import uk.gov.hmcts.ccd.sdk.api.Event;
 import uk.gov.hmcts.ccd.sdk.api.Permission;
 import uk.gov.hmcts.reform.cosapi.common.AddSystemUpdateRole;
+import uk.gov.hmcts.reform.cosapi.common.config.AppsConfig;
+import uk.gov.hmcts.reform.cosapi.constants.CommonConstants;
 import uk.gov.hmcts.reform.cosapi.edgecase.model.CaseData;
 import uk.gov.hmcts.reform.cosapi.edgecase.model.State;
 import uk.gov.hmcts.reform.cosapi.edgecase.model.UserRole;
+import uk.gov.hmcts.reform.cosapi.util.AppsUtil;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -27,26 +32,42 @@ import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.ccd.sdk.api.Permission.C;
 import static uk.gov.hmcts.ccd.sdk.api.Permission.R;
 import static uk.gov.hmcts.ccd.sdk.api.Permission.U;
-import static uk.gov.hmcts.reform.cosapi.constants.CommonConstants.CREATE_CASE_EVENT_ID;
-import static uk.gov.hmcts.reform.cosapi.edgecase.model.UserRole.CASE_WORKER;
+import static uk.gov.hmcts.reform.cosapi.constants.CommonConstants.PRL_CASE_TYPE;
 import static uk.gov.hmcts.reform.cosapi.edgecase.model.UserRole.CITIZEN;
 import static uk.gov.hmcts.reform.cosapi.util.ConfigTestUtil.createCaseDataConfigBuilder;
 import static uk.gov.hmcts.reform.cosapi.util.ConfigTestUtil.getEventsFrom;
+import static uk.gov.hmcts.reform.cosapi.util.TestConstant.CASE_DATA_FGM_ID;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
+@TestPropertySource("classpath:application.yaml")
 @ActiveProfiles("test")
 class CreateCaseEventTest {
-
-    @Mock
-    private AddSystemUpdateRole addSystemUpdateRole;
 
     @InjectMocks
     private CreateCaseEvent createCaseEvent;
 
+    @Mock
+    private AddSystemUpdateRole addSystemUpdateRole;
+
+    @Mock
+    private AppsConfig appsConfig;
+
+    @Mock
+    private AppsConfig.AppsDetails fgmAppDetail;
+
     @BeforeEach
     public void setUp() {
         MockitoAnnotations.openMocks(this);
+        fgmAppDetail = new AppsConfig.AppsDetails();
+        fgmAppDetail.setCaseType(PRL_CASE_TYPE);
+        fgmAppDetail.setJurisdiction(CommonConstants.PRL_JURISDICTION);
+        fgmAppDetail.setCaseTypeOfApplication(List.of(CASE_DATA_FGM_ID));
+
+        AppsConfig.EventsConfig eventsConfig = new AppsConfig.EventsConfig();
+        eventsConfig.setCreateEvent("citizen-prl-create-dss-application");
+
+        fgmAppDetail.setEventIds(eventsConfig);
 
     }
 
@@ -57,21 +78,21 @@ class CreateCaseEventTest {
         when(addSystemUpdateRole.addIfConfiguredForEnvironment(anyList()))
             .thenReturn(List.of(CITIZEN));
 
-        when(addSystemUpdateRole.isEnvironmentAat())
-            .thenReturn(true);
+        when(appsConfig.getApps()).thenReturn(Arrays.asList(fgmAppDetail));
 
         createCaseEvent.configure(configBuilder);
 
         assertThat(getEventsFrom(configBuilder).values())
             .extracting(Event::getId)
-            .contains(CREATE_CASE_EVENT_ID);
+            .contains(AppsUtil.getExactAppsDetailsByCaseType(appsConfig, PRL_CASE_TYPE).getEventIds()
+                          .getCreateEvent());
 
         SetMultimap<UserRole, Permission> expectedRolesAndPermissions =
             ImmutableSetMultimap.<UserRole, Permission>builder()
-            .put(CITIZEN, C)
-            .put(CITIZEN, R)
-            .put(CITIZEN, U)
-            .build();
+                .put(CITIZEN, C)
+                .put(CITIZEN, R)
+                .put(CITIZEN, U)
+                .build();
 
         assertThat(getEventsFrom(configBuilder).values())
             .extracting(Event::getGrants)
@@ -83,13 +104,15 @@ class CreateCaseEventTest {
         final ConfigBuilderImpl<CaseData, State, UserRole> configBuilder = createCaseDataConfigBuilder();
 
         when(addSystemUpdateRole.addIfConfiguredForEnvironment(anyList()))
-            .thenReturn(List.of(CITIZEN, CASE_WORKER));
+            .thenReturn(List.of(CITIZEN));
+
+        when(appsConfig.getApps()).thenReturn(Arrays.asList(fgmAppDetail));
 
         createCaseEvent.configure(configBuilder);
 
         assertThat(getEventsFrom(configBuilder).values())
             .extracting(Event::getId)
-            .contains(CREATE_CASE_EVENT_ID);
+            .contains("citizen-prl-create-dss-application");
 
         assertThat(getEventsFrom(configBuilder).values())
             .extracting(Event::getDescription)
@@ -97,13 +120,10 @@ class CreateCaseEventTest {
 
         SetMultimap<UserRole, Permission> expectedRolesAndPermissions =
             ImmutableSetMultimap.<UserRole, Permission>builder()
-            .put(CITIZEN, C)
-            .put(CITIZEN, R)
-            .put(CITIZEN, U)
-            .put(CASE_WORKER, C)
-            .put(CASE_WORKER, R)
-            .put(CASE_WORKER, U)
-            .build();
+                .put(CITIZEN, C)
+                .put(CITIZEN, R)
+                .put(CITIZEN, U)
+                .build();
 
         assertThat(getEventsFrom(configBuilder).values())
             .extracting(Event::getGrants)
