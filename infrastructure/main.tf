6eef661659f2ec93fd4d154e0ed3568e89a3e814
jaberelfasi <jaberelfasi@gmail.com>
2022-09-28 20:09:20 +0100

    initiating sptribs-dss-backend pipeline

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -16,13 +16,13 @@ data "azurerm_key_vault" "s2s_vault" {
   resource_group_name = "rpe-service-auth-provider-${var.env}"
 }
 
-data "azurerm_key_vault_secret" "microservicekey_sptribs_dss_backend" {
-  name         = "microservicekey-sptribs-dss-backend"
+data "azurerm_key_vault_secret" "microservicekey_fis_cos_api" {
+  name         = "microservicekey-fis-cos-api"
   key_vault_id = data.azurerm_key_vault.s2s_vault.id
 }
 
 resource "azurerm_key_vault_secret" "s2s-secret-sptribs-dss-backend" {
   name         = "s2s-secret-sptribs-dss-backend"
-  value        = data.azurerm_key_vault_secret.microservicekey_sptribs_dss_backend.value
+  value        = data.azurerm_key_vault_secret.microservicekey_fis_cos_api.value
   key_vault_id = data.azurerm_key_vault.sptribs_key_vault.id
 }
