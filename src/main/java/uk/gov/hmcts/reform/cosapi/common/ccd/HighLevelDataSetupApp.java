4e78e8006e4c018ba7cf5d592d8c6e65b55113c3
jaberelfasi <jaberelfasi@gmail.com>
2022-10-07 11:55:16 +0100

    Raising test PR

--- src/main/java/uk/gov/hmcts/reform/cosapi/common/ccd/HighLevelDataSetupApp.java
+++ src/main/java/uk/gov/hmcts/reform/cosapi/common/ccd/HighLevelDataSetupApp.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import uk.gov.hmcts.befta.dse.ccd.CcdEnvironment;
 import uk.gov.hmcts.befta.dse.ccd.CcdRoleConfig;
 import uk.gov.hmcts.befta.dse.ccd.DataLoaderToDefinitionStore;
+import uk.gov.hmcts.befta.util.BeftaUtils;
 
 import java.util.List;
 import java.util.Locale;
@@ -62,5 +63,11 @@ public class HighLevelDataSetupApp extends DataLoaderToDefinitionStore {
         return List.of(
             "build/ccd-config/ccd-" + "PRLAPPS" + "-" + environmentName + ".xlsx"
         );
+    }
+
+    @Override
+    public void createRoleAssignments() {
+        // Do not create role assignments.
+        BeftaUtils.defaultLog("Will NOT create role assignments!");
     }
 }
